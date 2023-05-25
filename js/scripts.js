081a2cf79ce79de8c2f7c8f93ed29e97bafd5b19
Amangoua Ange <adagou@yahoo.com>
2023-05-25 16:11:19 +0000

    updating files

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 let contrastToggle = false
+let darkBtn = document.querySelector('.darkBtn')
 
 function toggleContrast() {
+  darkBtn.classList.toggle('dark-theme')
+  // darkBtn.classList = 'fas fa-toggle-on'
+
+  darkBtn.removeAttribute('disabled')
   contrastToggle = !contrastToggle
   if (contrastToggle) {
     document.body.classList += ' dark-theme'
